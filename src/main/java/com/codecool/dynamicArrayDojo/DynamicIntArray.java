9609e1fa7b0a98c038ff4a01c97f3a5bb7936278
Piotr Górski <piotrek.gorski90@gmail.com>
2018-04-30 20:25:53 +0200

    Create toString and add method to pass 2 tests

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,5 +1,7 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.Arrays;
+
 class DynamicIntArray {
 
     private int[] array;
@@ -9,11 +11,24 @@ class DynamicIntArray {
     }
 
     DynamicIntArray() {
-        int arraySize = 5;
-        this.array = new int[arraySize];
+        this.array = new int[0];
         }
 
-    void add(int value){}
+    void add(int value){
+        array = Arrays.copyOf(array, array.length +1);
+        int size = array.length-1;
+        array[size] = value;
+    }
     void remove(int value){}
     void insert(int index, int value){}
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (int resultValue : array) {
+            result.append(" ");
+            result.append(resultValue);
+        }
+        return result.toString();
+    }
 }
